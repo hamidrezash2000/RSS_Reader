3520e81091e5606e6e218e6781bceee080887a06
Hamidreza Sharifzadeh <Hamidrezash2000@gmail.com>
2019-06-30 16:39:34 +0430

    InsertFeed function added in DB class

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,4 +1,8 @@
+
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import java.net.URL;
 
 public class DB {
     private Sql2o sql2o;
@@ -9,6 +13,19 @@ public class DB {
     }
 
     private DB() {
-        sql2o = new Sql2o("jdbc:mysql://127.0.0.1:3306/mysql", "root", "468729");
+        sql2o = new Sql2o("jdbc:mysql://127.0.0.1:3306/rss", "username", "12345678");
+    }
+
+    public void InsertFeed(String title, URL url) {
+        String insertSql =
+                "insert into feeds(title, url) " +
+                        "values (:title, :url)";
+        //todo : validate url
+        try (Connection con = sql2o.open()) {
+            con.createQuery(insertSql)
+                    .addParameter("title", title)
+                    .addParameter("url", url.toString())
+                    .executeUpdate();
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            DB.getInstance().InsertFeed("MehrNews", new URL("https://www.mehrnews.com/rss"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
