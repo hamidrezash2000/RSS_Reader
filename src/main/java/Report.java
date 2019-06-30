6eec70263f07fcebf0b5013e70da466aa1bb444e
Hamidreza Sharifzadeh <Hamidrezash2000@gmail.com>
2019-06-30 15:07:55 +0430

    Sample Code added

--- src/main/java/Report.java
+++ src/main/java/Report.java
@@ -1,7 +1,9 @@
 import java.net.URL;
+import java.util.Date;
 
 public class Report {
     private String title;
     private URL link;
+    private Date publishedDate;
     private String description;
 }
