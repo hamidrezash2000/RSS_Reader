2ba75e21f4117aca00a020aa48f9b56bedf52f6d
Kazem Faghih <kazemf78@yahoo.com>
2019-07-02 15:08:04 +0430

    create tables before tests and delete them after

--- src/test/DBTest.java
+++ src/test/DBTest.java
@@ -1,7 +1,20 @@
-import org.junit.Test;
+import org.junit.*;
+
+import java.util.Properties;
+
 import static junit.framework.TestCase.assertTrue;
 
 public class DBTest {
+
+    static final String createFeedTableQuery = "CREATE TABLE feeds (id INTEGER PRIMARY KEY AUTO_INCREMENT, title TEXT, url TEXT)";
+    static final String createReportTableQuery = "CREATE TABLE reports (id INTEGER PRIMARY KEY AUTO_INCREMENT, link TEXT , title TEXT , pubDate DATETIME, description TEXT , feedId INTEGER NOT NULL)";
+
+    @BeforeClass
+    public static void createTables () {;
+        DB.getInstanceForTest().executeQueryOnTest(createFeedTableQuery);
+        DB.getInstanceForTest().executeQueryOnTest(createReportTableQuery);
+    }
+
     @Test
     public void insertFeedTest() {
         Feed feed = new Feed("Test Title", "http://TestURL.URL");
@@ -14,5 +27,11 @@ public class DBTest {
         Report report = new Report(1, "Test Report Title", "http://TestURL.URL");
         DB.getInstanceForTest().insertReport(report);
         assertTrue(DB.getInstanceForTest().getAllReports().contains(report));
+    }
+
+    @AfterClass
+    public static void deleteTables() {
+        DB.getInstanceForTest().executeQueryOnTest("DROP TABLE feeds");
+        DB.getInstanceForTest().executeQueryOnTest("DROP TABLE reports");
     }
 }
