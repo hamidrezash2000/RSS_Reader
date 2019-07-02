6bb661b044ee692995c03c9af2e41cba893f0bea
Hamidreza Sharifzadeh <Hamidrezash2000@gmail.com>
2019-07-02 11:59:25 +0430

    add insertFeed Test in DBTest Class

--- src/test/DBTest.java
+++ src/test/DBTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static junit.framework.TestCase.assertTrue;
 
 public class DBTest {
+    @Test
+    public void insertFeedTest() {
+        Feed feed = new Feed("Test Title", "http://TestURL.URL");
+        DB.getInstanceForTest().insertFeed(feed);
+        assertTrue(DB.getInstanceForTest().getAllFeeds().contains(feed));
+    }
 }
